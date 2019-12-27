13c9c0ca7331495bbcaf857319b1d21e58ffc220
Mélissa Morin <melissa.morinmangaillou@gmail.com>
2019-12-27 10:06:31 -0500

    message et flocon

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Bienvenue</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -13,8 +13,15 @@
                             {{ session('status') }}
                         </div>
                     @endif
+                    <form action="/ajout" method="post">
+                        @csrf
+                        <div class="form-group">
+                            <label for="faits">Message</label>
+                            <input class="form-control" type="text" name="faits" id="faits">
+                        </div>
+                        <input class="btn btn-info w-100" type="submit" value="Envoyer">
+                    </form>
 
-                    You are logged in!
                 </div>
             </div>
         </div>
